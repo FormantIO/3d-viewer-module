01b53d4882d33b47b58f52fc3436d947fcc2ac9a
Richard Anaya <richard@formant.io>
2023-01-29 13:42:35 -0800

    postprocessing

--- src/Universe.tsx
+++ src/Universe.tsx
@@ -3,6 +3,13 @@ import { OrbitControls } from "@react-three/drei";
 import React from "react";
 import { Axis } from "./components/Axis";
 import { FormantColors } from "./FormantColors";
+import {
+  EffectComposer,
+  DepthOfField,
+  Bloom,
+  Noise,
+  Vignette,
+} from "@react-three/postprocessing";
 
 type IUniverseProps = {
   children?: React.ReactNode;
@@ -14,6 +21,17 @@ export function Universe(props: IUniverseProps) {
       <color attach="background" args={[FormantColors.flagship]} />
       <OrbitControls />
       <group rotation={[Math.PI / 2, 0, 0]}>{props.children}</group>
+      <EffectComposer>
+        <DepthOfField
+          focusDistance={0}
+          focalLength={0.02}
+          bokehScale={2}
+          height={480}
+        />
+        <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+        <Noise opacity={0.02} />
+        <Vignette eskil={false} offset={0.1} darkness={1.1} />
+      </EffectComposer>
     </Canvas>
   );
 }
