059ba7f069783d1821d58f114274ac9430a71056
Richard Anaya <richard@formant.io>
2023-02-07 15:08:43 -0800

    removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { Viewer } from "./Viewer";
 import { Demo } from "./Demo";
-import { ViewerApp } from "./ViewerApp";
 
 const query = new URLSearchParams(window.location.search);
 const demoMode = query.get("auth") === null;
-const standAloneMode = query.get("standalone") === "true";
 const app = document.getElementById("root");
 if (app) {
-  createRoot(app).render(
-    standAloneMode ? <ViewerApp /> : demoMode ? <Demo /> : <Viewer />
-  );
+  createRoot(app).render(demoMode ? <Demo /> : <Viewer />);
 }
