8cb62eb8fe57115e2e74bd7b731c8f7b0ac717ae
Richard Anaya <richard@formant.io>
2023-02-14 12:36:16 -0800

    typo

--- src/layers/common/LayerTypes.ts
+++ src/layers/common/LayerTypes.ts
@@ -1,6 +1,8 @@
-// these alyer types represent special layer meta data that is used to
-// for module level applications
+// these layer types represent special layer meta data that is used to
+// for proper UI interactions
 export enum LayerType {
+  // A container layer is a layer that contains other layers
+  // Clicking on it show go to it's first child layer
   CONTAINER = "container",
   // A trackable layer is a layer that is most likely to be used
   // tracked by the camera
