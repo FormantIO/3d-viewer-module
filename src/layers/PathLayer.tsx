166dd478eda87fc23097b0d0bbd8027b85cc6922
flaaudwls <74338977+flaaudwls@users.noreply.github.com>
2023-03-16 09:58:17 -0400

    Dev 4384 path width (#19)
    * add path width
    
    * update
    
    * Update PathLayer.tsx
    
    * update path width

--- src/layers/PathLayer.tsx
+++ src/layers/PathLayer.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { IUniverseLayerProps } from "./types";
 import { UniverseDataContext } from "./common/UniverseDataContext";
 import { LayerContext } from "./common/LayerContext";
@@ -8,17 +8,18 @@ import * as THREE from "three";
 import { IUniversePath } from "@formant/universe-core/dist/types/universe-core/src/model/IUniversePath";
 import { transformMatrix } from "./utils/transformMatrix";
 import { FormantColors } from "./utils/FormantColors";
+import { Line } from "@react-three/drei";
 
 interface ILocalPathProps extends IUniverseLayerProps {
   dataSource?: UniverseTelemetrySource;
-  pathWidth: number;
 }
 
 export const PathLayer = (props: ILocalPathProps) => {
-  const { dataSource, pathWidth } = props;
+  const { dataSource } = props;
   const universeData = useContext(UniverseDataContext);
   const layerData = useContext(LayerContext);
-  const [obj, setObj] = useState(new THREE.Mesh());
+  const [points, setPoints] = useState<THREE.Vector3[]>([]);
+  const groupRef = useRef<THREE.Group>(null!);
   useEffect(() => {
     if (!layerData) return;
 
@@ -40,37 +41,28 @@ export const PathLayer = (props: ILocalPathProps) => {
 
         const worldToLocalMatrix = transformMatrix(worldToLocal);
 
-        const points = positions.map(
-          (pos) => new THREE.Vector3(pos.x, pos.y, pos.z)
+        setPoints(
+          positions.map((pos) => new THREE.Vector3(pos.x, pos.y, pos.z))
         );
 
-        const w = pathWidth > 10 ? 10 : pathWidth < 1 ? 1 : pathWidth;
-
-        const curve = new THREE.CatmullRomCurve3(points);
-        const pathGeometry = new THREE.TubeGeometry(
-          curve,
-          20,
-          (0.01 / 2) * w,
-          8
-        );
-        const pathMaterial = new THREE.MeshBasicMaterial();
-        pathMaterial.color = new THREE.Color(FormantColors.blue);
-        const pathMesh = new THREE.Mesh(pathGeometry, pathMaterial);
-        pathMesh.matrixAutoUpdate = false;
-        pathMesh.matrix.copy(worldToLocalMatrix);
-
-        setObj(pathMesh);
+        const group = groupRef.current;
+        group.matrixAutoUpdate = false;
+        group.matrix.copy(worldToLocalMatrix);
       }
     );
 
     return () => {
       unsubscribe();
     };
-  }, [layerData, universeData, setObj]);
+  }, [layerData, universeData, setPoints]);
 
   return (
     <DataVisualizationLayer {...props} iconUrl="icons/3d_object.svg">
-      <primitive object={obj} />
+      <group ref={groupRef}>
+        {points.length > 0 && (
+          <Line points={points} lineWidth={10} color={FormantColors.blue} />
+        )}
+      </group>
     </DataVisualizationLayer>
   );
 };
