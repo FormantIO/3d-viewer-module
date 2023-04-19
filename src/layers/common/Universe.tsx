20937fade523e8deaa4f14a82e03f5180a2ca2ed
Andrey Mazepas <andrey@formant.io>
2023-04-19 11:43:48 -0300

    lower dpr for performance

--- src/layers/common/Universe.tsx
+++ src/layers/common/Universe.tsx
@@ -174,7 +174,7 @@ export function Universe(props: IUniverseProps) {
             onCreated={(state) => {
               setScene(state.scene);
             }}
-            dpr={[1, 2]}
+            dpr={1}
             flat
           >
             <XR>
